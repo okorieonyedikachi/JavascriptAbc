8cbbe0820c7649b6047692d45411ddff56395a1c
FoureyedVeen <cosmasdaniella@gmail.com>
2022-06-23 07:51:16 +0100

    Js 101

---
+++ index.js
@@ -0,0 +1,12 @@
+alert ('Hello');
+// let message;
+// message = 'Hello';
+// console.log(message);
+ 
+const name = 'John';   //string data type
+const age = 30; //number data type
+const isBrave = true; //boolean data type
+const x = null;
+const y = undefined;
+let z;
+
