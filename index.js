ad839d36e67ed3044ed18bbc1cdee35ff0b92be4
FoureyedVeen <cosmasdaniella@gmail.com>
2022-06-23 08:00:50 +0100

    types of quotes understood

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ alert ('Hello');
 // message = 'Hello';
 // console.log(message);
  
-const name = 'John';   //string data type
-const age = 30; //number data type
-const isBrave = true; //boolean data type
-const x = null;
-const y = undefined;
-let z;
+// const name = 'John';   //string data type
+// const age = 30; //number data type
+// const isBrave = true; //boolean data type
+// const x = null;
+// const y = undefined;
+// let z;
 
+let str ='Hello';
+let str2 = "Double quotes are ok too";
+let phrase = `backticks works ${str2}`
+alert (`Hello ${str2}`)
