3f2092eed86d44e4733f38a7bdb475cd555d1a96
FoureyedVeen <cosmasdaniella@gmail.com>
2022-06-24 22:38:58 +0100

    Basic operators

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ alert ('Hello');
 // const y = undefined;
 // let z;
 
-let str ='Hello';
-let str2 = "Double quotes are ok too";
-let phrase = `backticks works ${str2}`
-alert (`Hello ${str2}`)
+// let str ='Hello';
+// let str2 = "Double quotes are ok too";
+// let phrase = `backticks works ${str2}`
+// alert (`Hello ${str2}`)
+
+let age = prompt('how are old are you', 100);
+alert (`You are ${age} years old`);
+let isBoss = confirm ("Are you the boss?");
+let name = prompt('What is your name?', '');
+alert (name);
+
+let x = 1;
+x = -x;
+alert ( x ); //unary operator
+
+let x = 1, y = 4;
+alert ( y - x ); //binary operator
