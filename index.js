f75351bc1009d109a5993de5a066636700399fc2
FoureyedVeen <cosmasdaniella@gmail.com>
2022-07-13 10:23:11 +0100

    Task done, understanding the switch statement and syntax

--- index.js
+++ index.js
@@ -93,7 +93,35 @@ alert ('Hello');
 //  else   {
 //     alert ("I don't know you");
 // }
-let firstName = null;
-let lastName = null;
-let nickName = "Raveen";
-alert (firstName ?? lastName ?? nickName ?? 'Anonymous'); //Nullish Coalescing operator
+// let firstName = null;
+// let lastName = null;
+// let nickName = "Raveen";
+// alert (firstName ?? lastName ?? nickName ?? 'Anonymous'); //Nullish Coalescing operator
+
+// Understanding the switch statement and syntax
+let a = +prompt('a?', '');
+switch (a) {
+    case 0:
+     alert ( 0 );
+     break;
+    case 1:
+        alert ( 1 );
+        break;
+    case 2:
+    case 3:
+        alert( '2,3' );
+        break;   
+}
+if ( browser == 'Edge' ) {
+    alert ( "You've got the Edge!" );
+}
+else if ( browser == 'Chrome'
+|| bowser == 'Firefox'
+|| browser == 'Safari'
+|| browser == 'Opera'
+){
+    alert ( 'Okay we support these browsers too' );
+}
+else {
+    alert ( 'We hope that this page looks ok!' );
+}
